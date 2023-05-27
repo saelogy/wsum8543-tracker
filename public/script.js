3bb30ff5a14305af56281d9d9dd19b62b32493f0
saelogy <131426330+saelogy@users.noreply.github.com>
2023-05-27 13:47:20 +1000

    click on individual list item

--- public/script.js
+++ public/script.js
@@ -24,7 +24,7 @@ form.addEventListener("submit", function (event) {
 
     console.log(form.elements.mTitle.value)
 
-    addTask(
+    addMovie(
         form.elements.mTitle.value,
         form.elements.mDuration.value,
         form.elements.mGenre.value,
@@ -35,102 +35,78 @@ form.addEventListener("submit", function (event) {
     console.log(movieList)
 })
 
-function displayTask(task) {
+
+function showMovies(movie) {
     let item = document.createElement("li");
-    item.setAttribute("data-id", task.id);
-    item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
+    item.setAttribute("data-id", movie.id);
+    item.innerHTML = `<p id="title"><strong>${movie.title}</strong><br>${movie.duration}</p>`;
+    item.addEventListener("click", function(){
+      item.classList.toggle("done");
+    })
 
     movielist.appendChild(item);
 
-    // Clear the value of the input once the task has been added to the page
+    // Clear the value of the input once the movie has been added to the page
     form.reset();
 
     // Setup delete button DOM elements
     let delButton = document.createElement("button");
     let delButtonText = document.createTextNode("Delete");
     delButton.appendChild(delButtonText);
-    item.appendChild(delButton); // Adds a delete button to every task
+    item.appendChild(delButton); // Adds a delete button to every movie
 
     // Listen for when the delete button is clicked
     delButton.addEventListener("click", function (event) {
 
-        movieList.forEach(function (taskArrayElement, taskArrayIndex) {
-            if (taskArrayElement.id == item.getAttribute('data-id')) {
-                movieList.splice(taskArrayIndex, 1)
+        movieList.forEach(function (movieArrayElement, movieArrayIndex) {
+            if (movieArrayElement.id == item.getAttribute('data-id')) {
+                movieList.splice(movieArrayIndex, 1)
             }
         })
 
         // Make sure the deletion worked by logging out the whole array
         console.log(movieList)
 
-        item.remove(); // Remove the task item from the page when button clicked
-        // Because we used 'let' to define the item, this will always delete the right element
+        item.remove(); // Remove the movie item from the page when button clicked
 
     })
 
+    item.addEventListener("click", function(){
+      item.classList.remove("hidden")
+    })
 
 }
 
 
-// Create an object called 'task'
-// Populate the properties based on the provided data model
-
-// Commented out now the object creation is included in the function
-
-// var task = {
-//   name: "Initial Sketches",
-//   type: "Concept Ideation",
-//   id: Date.now(),
-//   date: new Date().toISOString(),
-//   rate: 50,
-//   time: 5,
-//   client: "Google"
-// }
-
-// console.log(task);
-
-
-// Create an array called 'taskList'
+// Create an array called 'movieList'
 var movieList = [];
 
-// Create a function called 'addTask'
-// Give the function input parameters for: name, type, rate, time, client
+// Create a function called 'addMovie'
 // Paste your object definition from above in the function
 // Replace the property values with the input paramaters
-// Add the object to the taskList array
+// Add the object to the movieList array
 
-function addTask(name, type, rate, time, client) {
-
-    // Creating the object with the usual property:value syntax
-    // Create task object 
-    // let task = {
-    //   name: name,
-    //   type: type,
-    //   id: Date.now(),
-    //   date: new Date().toISOString(),
-    //   rate: rate,
-    //   time: time,
-    //   client: client
-    // }
-
+function addMovie(title, duration, genre, status, emotion, notes) {
     // Creating the object, directly passing in the input parameters
-    let task = {
-        name,
-        type,
+    let movie = {
+        title,
+        duration,
         id: Date.now(),
         date: new Date().toISOString(),
-        rate,
-        time,
-        client
+        genre,
+        status,
+        emotion,
+        notes
     }
 
-    movieList.push(task);
-    displayTask(task);
+    movieList.push(movie);
+    showMovies(movie);
 
 }
 
 // Call the function with test values for the input paramaters
-addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
+//////// REMOVE AT THE END.
+addMovie("Initial Sketches", "Concept Ideation", 50, 5, "Google");
 
 // Log the array to the console.
 console.log(movieList);
