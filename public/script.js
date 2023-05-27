67d9392ac4b7118a0819857d569058703954fbb2
saelogy <131426330+saelogy@users.noreply.github.com>
2023-05-27 15:01:21 +1000

    basic functionalities

--- public/script.js
+++ public/script.js
@@ -20,60 +20,71 @@ const form = document.getElementById("mForm");
 const movielist = document.getElementById("movielist");
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    console.log(form.elements.mTitle.value)
+  console.log(form.elements.mTitle.value)
 
-    addMovie(
-        form.elements.mTitle.value,
-        form.elements.mDuration.value,
-        form.elements.mGenre.value,
-        form.elements.mWatched.value,
-        form.elements.mEmotion.value,
-        form.elements.mNotes.value,
-    )
-    console.log(movieList)
+  addMovie(
+    form.elements.mTitle.value,
+    form.elements.mDuration.value,
+    form.elements.mGenre.value,
+    form.elements.complete.value,
+    form.elements.mEmotion.value,
+    form.elements.mNotes.value,
+  )
+  console.log(movieList)
 })
 
 
 function showMovies(movie) {
-    let item = document.createElement("li");
-    item.setAttribute("data-id", movie.id);
-    item.innerHTML = `<p id="title"><strong>${movie.title}</strong><br>${movie.duration}</p>`;
-    item.addEventListener("click", function(){
-      item.classList.toggle("done");
+  let item = document.createElement("li");
+  item.setAttribute("data-id", movie.id);
+
+  // object values: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+  if (Object.values(movie).includes("y")) {
+    console.log("watched");
+    item.innerHTML = `<span class="dot" id="y"></span><p id="title"><strong>${movie.title}</strong><br>${movie.duration}</p>`;
+  } else {
+    console.log("not watched");
+    item.innerHTML = `<span class="dot" id="n"></span><p id="title"><strong>${movie.title}</strong><br>${movie.duration}</p>`;
+  }
+
+  // item list popup: https://stackoverflow.com/questions/61169448/click-on-the-list-item-it-toggles-the-line-through-sytle-class-on-and-off
+  item.addEventListener("click", function () {
+    notepopup.classList.toggle("hidden");
+    document.getElementById("notepopup").innerHTML = `<em><h3>NOTE</h3></br><p>added on ${movie.date}</em><br>${movie.notes}</p>`;
+  })
+
+  movielist.appendChild(item);
+
+  // Clear the value of the input once the movie has been added to the page
+  form.reset();
+
+  // Setup delete button DOM elements
+  let delButton = document.createElement("button");
+  let delButtonText = document.createTextNode("Delete");
+  delButton.appendChild(delButtonText);
+  item.appendChild(delButton); // Adds a delete button to every movie
+
+  // Listen for when the delete button is clicked
+  delButton.addEventListener("click", function (event) {
+
+    movieList.forEach(function (movieArrayElement, movieArrayIndex) {
+      if (movieArrayElement.id == item.getAttribute('data-id')) {
+        movieList.splice(movieArrayIndex, 1)
+      }
     })
 
-    movielist.appendChild(item);
+    // Make sure the deletion worked by logging out the whole array
+    console.log(movieList)
 
-    // Clear the value of the input once the movie has been added to the page
-    form.reset();
+    item.remove(); // Remove the movie item from the page when button clicked
 
-    // Setup delete button DOM elements
-    let delButton = document.createElement("button");
-    let delButtonText = document.createTextNode("Delete");
-    delButton.appendChild(delButtonText);
-    item.appendChild(delButton); // Adds a delete button to every movie
+  })
 
-    // Listen for when the delete button is clicked
-    delButton.addEventListener("click", function (event) {
-
-        movieList.forEach(function (movieArrayElement, movieArrayIndex) {
-            if (movieArrayElement.id == item.getAttribute('data-id')) {
-                movieList.splice(movieArrayIndex, 1)
-            }
-        })
-
-        // Make sure the deletion worked by logging out the whole array
-        console.log(movieList)
-
-        item.remove(); // Remove the movie item from the page when button clicked
-
-    })
-
-    item.addEventListener("click", function(){
-      item.classList.remove("hidden")
-    })
+  item.addEventListener("click", function () {
+    item.classList.remove("hidden")
+  })
 
 }
 
@@ -87,26 +98,28 @@ var movieList = [];
 // Add the object to the movieList array
 
 function addMovie(title, duration, genre, status, emotion, notes) {
-    // Creating the object, directly passing in the input parameters
-    let movie = {
-        title,
-        duration,
-        id: Date.now(),
-        date: new Date().toISOString(),
-        genre,
-        status,
-        emotion,
-        notes
-    }
+  // Creating the object, directly passing in the input parameters
+  let movie = {
+    title,
+    duration,
+    id: Date.now(),
+    // date format location code: https://stackoverflow.com/questions/27939773/tolocaledatestring-short-format
+    date: new Date().toLocaleDateString('tr-TR', { day: "numeric", year: "numeric", month: "short" }),
+    genre,
+    status,
+    emotion,
+    notes
+  }
 
-    movieList.push(movie);
-    showMovies(movie);
+  movieList.push(movie);
+  showMovies(movie);
 
 }
 
 // Call the function with test values for the input paramaters
 //////// REMOVE AT THE END.
-addMovie("Initial Sketches", "Concept Ideation", 50, 5, "Google");
+addMovie("a silent voice", "2:25", "romance", "y", "sadness", "made me cry 10/10");
+addMovie("a silent voice", "2:25", "romance", "n", "sadness", "made me cry 10/10");
 
 // Log the array to the console.
 console.log(movieList);
