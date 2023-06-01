7d5946dccb87fd91a39f02feb50769d837a8dbf2
saelogy <131426330+saelogy@users.noreply.github.com>
2023-06-01 15:52:38 +1000

    localStorage not showing on site

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # wsum8543-tracker
-## development log
+## Config & deployment
+The application contains three major files and :
+* index.html
+* script.js
+* style.css
+
+T
+
+The web application is built for screens with a standard PC width of 1920px and the mobile width of 600px. The height is set to designed to best run at the max PC height of 1080px. 
+As the web application uses Node, users will first have to run a command to install the dependencies with in their terminal with
+`npm install`
+Afterwards, to run the application, users will have to use 
+`npm run dev`
+
+## Development log
 ### May 12 2023
 The basic structure of the site is set up. Server, css, html and readme files. This did not include any coding work. The html file was a base with the default DOCTYPE setup, css and script files were empty (minus the server setup scripts).
 ### May 26 2023
@@ -8,18 +22,17 @@ With some simple javascript and html tweaking, basic functionalities such as pop
 ### May 27 2023
 All "basic" functionalities suck as clickable/expandable items and functioning forms, etc. are complete. I also played around with localStorage() to try and make session data and user records persist through multiple instances. However, I couldn't get it to work, when I think I managed to get it working (eg. using stringify to transform objects into strings in order to insert into an array to be saved by the browser), it messes up another part of the site (eg. the list of movies stopped displaying completely).
 ### May 29 2023
-Decided to move away from localStorage in order to start working on the visual design on the application. 
+Decided to move away from localStorage in order to start working on the visual design on the application. Since css files tend to be long, I tried to keep the structure organised by listing the elements from class to ids and from visually larger elements to smaller ones. However, this would end up being useless since some elements conflict each other's hierarchy. 
+I also changed some design elements of the app as I was unable to get them to behave the way I wanted them to; namely, the delete button. Initially, I had wanted the delete button to be situated inside the expanded item note box, however it would decrease its user friendliness as it would make deleting an item two clicks instead of one. 
 ### May 30 2023
+
+### May 31 2023
+
 
 To document the development process, you will need to create a log of your work, highlighting any iterations and improvements on the designs. 
 
-## config & deployment
-The web application is built for screens with a standard PC width of 1920px and the mobile width of 600px. The height is set to designed to best run at the max PC height of 1080px. 
-As the web application uses Node, users will first have to run a command to install the dependencies with in their terminal with
-`npm install`
-Afterwards, to run the application, users will have to use 
-`npm run dev`
+## Further Iterations
 
+## References
 
-
-The configuration could include information about what screen dimensions and resolutions are best for viewing your application. It should also include any extra information about interacting with your application, for example if there are any steps required to set up use of an API.
+![image](readmeimgs/img1.png)
--- public/script.js
+++ public/script.js
@@ -1,10 +1,16 @@
 // Create an array called 'movieList'
-var movieList = [];
+const movieList = [];
+
+function onLoad() {
+  localStorage.getItem('movies');
+  //localStorage.clear();
+}
+
+//console.log(localStorage.getItem('movies'))
 
 // form show + hide
 const popup = document.getElementById("popup");
 const showButton = document.getElementById("openForm");
-const hideButton = document.getElementById("hideElement");
 const notepopup = document.getElementById('notepopup');
 
 // Attach event listeners
@@ -28,98 +34,105 @@ form.addEventListener("submit", function (event) {
     form.elements.mNotes.value,
   )
 
-  console.log(movieList)
+  //console.log(movieList);
+  localStorage.setItem('movies', JSON.stringify(movieList));
+
 })
 
-function showMovies(movie) {
-  let item = document.createElement("li");
-  item.setAttribute("data-id", movie.id);
+function showMovies() {
 
-  // object values: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
-  if (Object.values(movie).includes("y")) {
-    item.innerHTML = `<div class="item"><span class="dot" id="y"></span><p id="title"><strong>${movie.title}</strong></p><br></div>`;
-  } else {
-    item.innerHTML = `<div class="item"><span class="dot" id="n"></span><p id="title"><strong>${movie.title}</strong></p><br></div>`;
-  } 
+  let movies = JSON.parse(localStorage.getItem('movies'));
 
-  // check for emotional rating +display as image
-  let imgEmotion = document.createElement('img');
-  imgEmotion.setAttribute('id','imgEmotion');
-  if (Object.values(movie).includes("joy")){
-    // img cred. https://www.flaticon.com/authors/freepik
-    imgEmotion.src='https://i.imgur.com/KvWgmWD.png';
-  } else if (Object.values(movie).includes("sadness")){
-    // img cred. https://www.flaticon.com/authors/freepik
-    imgEmotion.src='https://i.imgur.com/iD7c0oh.png';
-  } else if (Object.values(movie).includes("fear")){
-    // img cred. 
-    imgEmotion.src='https://i.imgur.com/rVoazbt.png';
-  } else if (Object.values(movie).includes("disgust")){
-    // img cred. https://www.flaticon.com/authors/those-icons
-    imgEmotion.src='https://i.imgur.com/oL0BxtZ.png';
-  } else if (Object.values(movie).includes("anger")){
-    // img cred. https://www.flaticon.com/authors/joalfa
-    imgEmotion.src='https://i.imgur.com/w7ckg2v.png';
-  }
+  console.log(movies);
 
-  item.appendChild(imgEmotion);
+  if (movies !== null) {
+    console.log(movies)
+    movies.forEach(function(movie) {
 
-  // check for genre + display as image
-  let imgGenre = document.createElement('img');
-  imgGenre.setAttribute('id','imgGenre');
-  if (Object.values(movie).includes("action")){
-    // img cred. https://www.flaticon.com/authors/freepik
-    imgGenre.src='https://i.imgur.com/KvWgmWD.png';
-  } else if (Object.values(movie).includes("comedy")){
-    // img cred. https://www.flaticon.com/authors/freepik
-    imgGenre.src='https://i.imgur.com/iD7c0oh.png';
-  } else if (Object.values(movie).includes("drama")){
-    // img cred. 
-    imgGenre.src='https://i.imgur.com/rVoazbt.png';
-  } else if (Object.values(movie).includes("fantasy")){
-    // img cred. https://www.flaticon.com/authors/those-icons
-    imgGenre.src='https://i.imgur.com/oL0BxtZ.png';
-  } else if (Object.values(movie).includes("horror")){
-    // img cred. https://www.flaticon.com/authors/joalfa
-    imgGenre.src='https://i.imgur.com/w7ckg2v.png';
-  } else if (Object.values(movie).includes("romance")){
-    // img cred. https://www.flaticon.com/authors/joalfa
-    imgGenre.src='https://i.imgur.com/w7ckg2v.png';
-  }
+      let item = document.createElement("li");
+      item.setAttribute("data-id", movie.id);
 
-  item.appendChild(imgGenre);
-
-  movielist.appendChild(item);
-
-  // Clear the value of the input once the movie has been added to the page
-  form.reset();
-
-  // Setup delete button DOM elements
-  let delButton = document.createElement('img');
-  delButton.setAttribute('id','delBtn');
-  // img cred. https://www.flaticon.com/authors/freepik
-  delButton.src = 'https://i.imgur.com/kkd7kac.png';
-  item.appendChild(delButton); // Adds a delete button to every movie
-
-  // Listen for when the delete button is clicked
-  delButton.addEventListener("click", function () {
-
-    movieList.forEach(function (movieArrayElement, movieArrayIndex) {
-      if (movieArrayElement.id == item.getAttribute('data-id')) {
-        movieList.splice(movieArrayIndex, 1)
+      // object values: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+      if (Object.values(movie).includes("y")) {
+        item.innerHTML = `<div class="item"><span class="dot" id="y"></span><p id="title"><strong>${movie.title}</strong></p><br></div>`;
+      } else {
+        item.innerHTML = `<div class="item"><span class="dot" id="n"></span><p id="title"><strong>${movie.title}</strong></p><br></div>`;
       }
+
+      // check for emotional rating +display as image
+      let imgEmotion = document.createElement('img');
+      imgEmotion.setAttribute('id', 'imgEmotion');
+      if (Object.values(movie).includes("joy")) {
+        // img cred. https://www.flaticon.com/authors/freepik
+        imgEmotion.src = 'https://i.imgur.com/KvWgmWD.png';
+      } else if (Object.values(movie).includes("sadness")) {
+        // img cred. https://www.flaticon.com/authors/freepik
+        imgEmotion.src = 'https://i.imgur.com/iD7c0oh.png';
+      } else if (Object.values(movie).includes("fear")) {
+        // img cred. 
+        imgEmotion.src = 'https://i.imgur.com/rVoazbt.png';
+      } else if (Object.values(movie).includes("disgust")) {
+        // img cred. https://www.flaticon.com/authors/those-icons
+        imgEmotion.src = 'https://i.imgur.com/oL0BxtZ.png';
+      } else if (Object.values(movie).includes("anger")) {
+        // img cred. https://www.flaticon.com/authors/joalfa
+        imgEmotion.src = 'https://i.imgur.com/w7ckg2v.png';
+      }
+
+      item.appendChild(imgEmotion);
+
+      // check for genre + display as image
+      let imgGenre = document.createElement('img');
+      imgGenre.setAttribute('id', 'imgGenre');
+      // all following img cred. https://www.flaticon.com/authors/freepik
+      if (Object.values(movie).includes("action")) {
+        imgGenre.src = 'https://i.imgur.com/KxMQ9oa.png';
+      } else if (Object.values(movie).includes("comedy")) {
+        imgGenre.src = 'https://i.imgur.com/LSCudQs.png';
+      } else if (Object.values(movie).includes("drama")) {
+        imgGenre.src = 'https://i.imgur.com/U5oMqty.png';
+      } else if (Object.values(movie).includes("fantasy")) {
+        imgGenre.src = 'https://i.imgur.com/6SineeK.png';
+      } else if (Object.values(movie).includes("horror")) {
+        imgGenre.src = 'https://i.imgur.com/scaknLP.png';
+      } else if (Object.values(movie).includes("romance")) {
+        imgGenre.src = 'https://i.imgur.com/9S0maUr.png';
+      }
+
+      item.appendChild(imgGenre);
+
+      movielist.appendChild(item);
+
+      // Clear the value of the input once the movie has been added to the page
+      form.reset();
+
+      // Setup delete button DOM elements
+      let delButton = document.createElement('img');
+      delButton.setAttribute('id', 'delBtn');
+      // img cred. https://www.flaticon.com/authors/freepik
+      delButton.src = 'https://i.imgur.com/kkd7kac.png';
+      item.appendChild(delButton); // Adds a delete button to every movie
+
+      // Listen for when the delete button is clicked
+      delButton.addEventListener("click", function () {
+
+        movieList.forEach(function (movieArrayElement, movieArrayIndex) {
+          if (movieArrayElement.id == item.getAttribute('data-id')) {
+            movieList.splice(movieArrayIndex, 1)
+          }
+        })
+
+        item.remove(); // Remove the movie item from the page when button clicked
+
+      })
+
+      // item list popup: https://stackoverflow.com/questions/61169448/click-on-the-list-item-it-toggles-the-line-through-sytle-class-on-and-off
+      item.addEventListener("click", function () {
+        notepopup.showModal();
+        document.getElementById("notepopup").innerHTML = `<div id="noscroll"><em><h3>NOTE</h3><p ="date">added on ${movie.date}</p><p id="duration">length ${movie.duration}</p></em></div><br><p id="notes">${movie.notes}</p><br><p id="esc">press esc to close</p>`;
+      })
     })
-
-    item.remove(); // Remove the movie item from the page when button clicked
-
-  })
-
-  // item list popup: https://stackoverflow.com/questions/61169448/click-on-the-list-item-it-toggles-the-line-through-sytle-class-on-and-off
-  item.addEventListener("click", function () {
-    notepopup.showModal();
-    document.getElementById("notepopup").innerHTML = `<div id="noscroll"><em><h3>NOTE</h3><p ="date">added on ${movie.date}</p><p id="duration">length ${movie.duration}</p></em></div><br><p id="notes">${movie.notes}</p><br><p id="esc">press esc to close</p>`;
-  })
-
+  }
 }
 
 // Add the object to the movieList array
@@ -139,15 +152,11 @@ function addMovie(title, duration, genre, status, emotion, notes) {
   }
 
   movieList.push(movie);
-  showMovies(movie);
+  showMovies();
 
 }
 
 // Call the function with test values for the input paramaters
-//////// REMOVE AT THE END.
-addMovie("a silent voice", "2:25", "romance", "y", "sadness", "made me cry 10/10 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-addMovie("evangelion", "2:25", "scifi", "n", "joy", "made me cry 10/10");
 
 
-// Log the array to the console.
-console.log(movieList);
+//console.log(movieList);
